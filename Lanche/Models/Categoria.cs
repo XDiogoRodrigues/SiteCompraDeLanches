29242e67ecd9d4d120167503e95e4a23d4fa9aa4
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-30 16:51:20 -0300

    Inicializado o projeto. Declarada as classes Lanche e Categoria

---
+++ Lanche/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace Lanche.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Name { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
---
+++ Lanche/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lanche.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
